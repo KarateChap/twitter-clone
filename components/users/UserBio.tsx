17d6eaeb42629340cc06012faae796bdfca4fec4
KarateChap <gabrielmarianoofficial@gmail.com>
2023-05-09 13:34:28 +0800

    fix follow unfollow bug

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,11 +1,13 @@
+import { useMemo } from "react";
+import { BiCalendar } from "react-icons/bi";
 import { format } from "date-fns";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
-import React, { useMemo } from "react";
-import Button from "../Button";
-import { BiCalendar } from "react-icons/bi";
+import useFollow from "@/hooks/useFollow";
 import useEditModal from "@/hooks/useEditModal";
+
+import Button from "../Button";
 
 interface UserBioProps {
   userId: string;
@@ -17,6 +19,8 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
 
   const editModal = useEditModal();
 
+  const { isFollowing, toggleFollow } = useFollow(userId);
+
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
       return null;
@@ -24,13 +28,19 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
 
     return format(new Date(fetchedUser.createdAt), "MMMM yyyy");
   }, [fetchedUser?.createdAt]);
+
   return (
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
-        {currentUser?.id == userId ? (
+        {currentUser?.id === userId ? (
           <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
-          <Button onClick={() => {}} label="Follow" secondary />
+          <Button
+            onClick={toggleFollow}
+            label={isFollowing ? "Unfollow" : "Follow"}
+            secondary={!isFollowing}
+            outline={isFollowing}
+          />
         )}
       </div>
       <div className="mt-8 px-4">
@@ -44,13 +54,13 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           <p className="text-white">{fetchedUser?.bio}</p>
           <div
             className="
-                flex
-                flex-row
-                items-center
-                gap-2
-                mt-4
-                text-neutral-500
-            "
+              flex 
+              flex-row 
+              items-center 
+              gap-2 
+              mt-4 
+              text-neutral-500
+          "
           >
             <BiCalendar size={24} />
             <p>Joined {createdAt}</p>
--- pages/api/users/[userId].ts
+++ pages/api/users/[userId].ts
@@ -1,18 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 import prisma from "@/libs/prismadb";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method != "GET") {
+  if (req.method !== "GET") {
     return res.status(405).end();
   }
 
   try {
     const { userId } = req.query;
 
-    if (!userId || typeof userId != "string") {
+    if (!userId || typeof userId !== "string") {
       throw new Error("Invalid ID");
     }
 
